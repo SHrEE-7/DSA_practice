bde66cd752c91829a58b3d5da7d22bbc357663ce
Shree <shreekumarf@gmail.com>
2022-07-30 22:37:21 +0530

    even odd number

---
+++ Basic/even_odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter number\n";
+    cin>>num;
+    if (num%2==0){
+        cout<<"Entered number is even\n";
+    }else{
+        cout<<"Entered number is odd\n";
+    }
+}
