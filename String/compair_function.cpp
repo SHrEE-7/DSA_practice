d35b843a98f575516252b8fc9c9b4df8d2900a98
Shree <shreekumarf@gmail.com>
2022-07-31 17:33:02 +0530

    compair function in string

---
+++ String/compair_function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    // compare function in string
+    string a1 = "abc";
+    string a2 = "abc";
+    cout<<a2.compare(a1)<<endl; // if both the strings are equal then output will be 0, else it will be any number.
+
+}
